e714ead3fed576a4e8537254d6d8faef04b7acb3
yasirshahid <66242488+yasirshahid@users.noreply.github.com>
2020-06-13 19:54:33 +0500

    Counter web add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Message from './message';
 import './App.css';
 
+
+  
 function App() {
+  let [count,setcount]=useState(0);
+  let [isMorning,setMorning]=useState(false);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={`box $ {isMorning ? 'dayLight' : " "}`}>
+      <h1>Day Time ={isMorning ? 'Morning' : 'Night'}</h1>
+       
+
+      <Message  counter={count}/>
+      <button onClick={()=>setcount(++count)}>
+        Value + </button>
+      <button onClick={()=>setcount(--count)}>
+        value - </button>
+      <br/>
+      <br/>
+      <button onClick={()=>setcount(0)}> 
+       Reset </button>
+      </div>
+
   );
 }
+
 
 export default App;
